68ce6cc0cf82c4666c304dc938887c1d8c355e6e
Daniel Spilker <daniel.spilker@bdess.com>
2025-05-14 10:05:51 +0000

    migrate config entries, avoid unnecessary type conversion

--- custom_components/solarman_api/__init__.py
+++ custom_components/solarman_api/__init__.py
@@ -8,7 +8,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import SolarmanApiClient
-from .const import CONF_APP_ID, CONF_APP_SECRET
+from .const import CONF_APP_ID, CONF_APP_SECRET, CONF_DEVICE_SERIAL_NUMBER, LOGGER
 from .coordinator import SolarmanCoordinator, SolarmanData
 
 _PLATFORMS: list[Platform] = [Platform.SENSOR]
@@ -40,3 +40,30 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
+
+
+async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Migrate old entry."""
+    LOGGER.debug(
+        "Migrating configuration from version %s.%s",
+        config_entry.version,
+        config_entry.minor_version,
+    )
+
+    if config_entry.version > 1:
+        # This means the user has downgraded from a future version
+        return False
+
+    if config_entry.version == 1:
+        new_data = {**config_entry.data}
+        new_data[CONF_DEVICE_SERIAL_NUMBER] = str(new_data[CONF_DEVICE_SERIAL_NUMBER])
+
+        hass.config_entries.async_update_entry(config_entry, data=new_data, version=2)
+
+    LOGGER.debug(
+        "Migration to configuration version %s.%s successful",
+        config_entry.version,
+        config_entry.minor_version,
+    )
+
+    return True
